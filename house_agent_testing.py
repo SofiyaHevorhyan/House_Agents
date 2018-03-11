11ec5e647840f5edab7ae2cf67d509fa8dacebbd
SofiyaHevorhyan <36001777+SofiyaHevorhyan@users.noreply.github.com>
2018-03-11 18:06:23 +0200

    Add files via upload

---
+++ house_agent_testing.py
@@ -0,0 +1,10 @@
+# File: house_agent_testing.py
+# Simple testing of the classes
+
+from house_agents import Agent
+
+agent = Agent()
+agent.add_property()
+
+agent.add_property()
+agent.display_properties()
